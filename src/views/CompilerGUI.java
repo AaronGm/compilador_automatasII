4608736e6fbeac8384fb67f18a02fc17d41357af
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2019-03-03 22:32:36 -0600

    Actualización de la vista del compilador!

--- src/views/CompilerGUI.java
+++ src/views/CompilerGUI.java
@@ -7,12 +7,18 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -21,11 +27,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.text.DefaultStyledDocument;
-import javax.swing.text.Style;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyleContext;
 
 /**
  *
@@ -33,20 +37,22 @@ import javax.swing.text.StyleContext;
  */
 public class CompilerGUI extends JFrame {
     private JPanel pnlRoot;
-    private JPanel pnlIzq;
     
-    private StyleContext ctx;
-        
-    private JTextPane txPane;
+    private JPanel pnlHead;
+    private JPanel pnlDer;
+    private JPanel pnlBajo;
+    
+    private JTextPane tpEditor;
     private JLabel lbNombreCompiler;
-    private JTable tbTokens;
-    private JList<String> lsErrores;
+    private JTextPane tpConsola;
     
-    private DefaultListModel modelList;
+    private JTable tbTokens;
+    
     private DefaultTableModel modelTable;
     
     private JScrollPane scrollTabla;
-    private JScrollPane scrollTextPane;
+    private JScrollPane scrollEditor;
+    private JScrollPane scrollConsola;
     
     private JButton btnCompilar;
     private JButton btnErrores;
@@ -61,8 +67,12 @@ public class CompilerGUI extends JFrame {
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize(); 
         int ancho = screen.width;
         int largo = screen.height;
-        setTitle("Compilador K'OZ");
+        setTitle("PIXEL Compiler");
+        URL iconUrl = getClass().getResource("/assets/img/icon48x48.png");
+        ImageIcon icon = new ImageIcon(iconUrl);
+        setIconImage(icon.getImage());
         setMinimumSize(new Dimension(((ancho / 3) * 2) + 300, largo - 60));
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         pack();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -71,96 +81,117 @@ public class CompilerGUI extends JFrame {
 
     private void initComponents() {
         pnlRoot = (JPanel) getContentPane();
-        pnlIzq = new JPanel(new GridBagLayout());
-        txPane = new JTextPane();
-        DefaultStyledDocument doc = (DefaultStyledDocument) txPane.getStyledDocument();
-        Style style = doc.addStyle("style", null);
-        StyleConstants.setForeground(style, Color.red);
         
-        txPane.setText("# Mi propio lang\n" +
-"var x = \"Hola\"\n" +
-"var y = \"Mundo\"\n" +
-"var r = 6 + 12\n" +
-"var f = 12 ** 2\n" +
-"var m = 12 | 2\n" +
-"var rela = 20 ** 2\n" +
-"const miConstante = 20.3\n" +
-"out -> x + y\n" +
-"outln -> 20+10\n" +
-"\"Esto es un pequeño texto que se me ocurrió\"\n" +
-"20 + 10\n" +
-"\n" +
-"x = \"Bola\"\n" +
-"\n" +
-"var fs=80+100\n" +
-"\n" +
-"20 + 20 * (31 - 10)");
+        pnlHead = new JPanel();
+        pnlDer = new JPanel(new BorderLayout());
+        pnlBajo = new JPanel(new BorderLayout());
         
-        lbNombreCompiler = new JLabel("K'OZ Compiler");
-        lsErrores = new JList<>();
+        tpEditor = new JTextPane();
+        tpEditor.setBorder(new EmptyBorder(24, 24, 24, 24));
+        tpEditor.setFont(inputFont());
+        
+        tpConsola = new JTextPane();
+        tpConsola.setEditable(false);
+        tpConsola.setFont(addFont("RobotoMono-Medium.ttf", 15));
+        tpConsola.setBackground(new Color(0x535459));
+        tpConsola.setForeground(Color.white);
+        
+        lbNombreCompiler = new JLabel("Pixel Lang");
+        lbNombreCompiler.setFont(titleFont(21));
         tbTokens = new JTable();
         
-        tbTokens.setFont(new Font("Arial", Font.TRUETYPE_FONT, 14));
+        tbTokens.setFont(inputFont());
         
-        modelList = new DefaultListModel();
         modelTable = new DefaultTableModel(){
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
         };
-        btnCompilar = new JButton("Compilar");
-        btnErrores = new JButton("Ver errores");
         
-        initPnlIzq();
+        btnCompilar = new JButton("Compilar");
+        btnCompilar.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/assets/img/play-icon.png")).getImage().getScaledInstance(18, 18, Image.SCALE_SMOOTH)));
+        
+        btnErrores = new JButton("Errores");
+        btnErrores.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/assets/img/debug.png")).getImage().getScaledInstance(18, 18, Image.SCALE_SMOOTH)));
+        
+        scrollTabla = new JScrollPane(tbTokens);
+        scrollEditor = new JScrollPane(tpEditor);
+        scrollConsola = new JScrollPane(tpConsola);
+        
+        estilosJScrollPanes(scrollConsola, scrollEditor, scrollTabla);
+        initPnlHead();
+        initPnlDer();
+        initPnlBajo();
         initTablaTokens();
-        initTxPane();
     }
     
-    private void initPnlIzq() {
-        GridBagConstraints c = new GridBagConstraints();
-        pnlIzq.add(btnCompilar, c);
-        pnlIzq.add(btnErrores, c);
+    private void initPnlHead() {
+        pnlHead.setLayout(new BoxLayout(pnlHead, BoxLayout.X_AXIS));
+        pnlHead.setBackground(Color.white);
+        pnlHead.setBorder(new EmptyBorder(16, 24, 16, 24));
+        
+        pnlHead.add(lbNombreCompiler);
+        pnlHead.add(Box.createGlue());
+        pnlHead.add(btnCompilar);
+        pnlHead.add(Box.createHorizontalStrut(16));
+        pnlHead.add(btnErrores);
+    }
+    
+    private void initPnlDer() {
+        pnlDer.setPreferredSize(new Dimension(460, 0));
+        pnlDer.add(BorderLayout.CENTER, tpConsola);
     }
     
     private void initTablaTokens() {
-        scrollTabla = new JScrollPane(tbTokens);
-        scrollTabla.setPreferredSize(new Dimension(0, 250));
+        scrollTabla.setPreferredSize(new Dimension(0, 200));
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
         modelTable.setColumnIdentifiers(new String[] { "Token", "Descripción" });
         tbTokens.setModel(modelTable);
-    }
-    
-    private void initTxPane() {
-        scrollTextPane = new JScrollPane(txPane);
-        txPane.setFont(inputFont());
-        
+        tbTokens.getColumnModel().getColumn(0).setCellRenderer(centerRenderer);
+        tbTokens.getColumnModel().getColumn(0).setMinWidth(340);
+        tbTokens.getColumnModel().getColumn(0).setMaxWidth(340);
     }
     
     private void setComponents() {
-        pnlRoot.add(BorderLayout.NORTH, lbNombreCompiler);
-        pnlRoot.add(BorderLayout.CENTER, scrollTextPane);
-        pnlRoot.add(BorderLayout.WEST, pnlIzq);
-        pnlRoot.add(BorderLayout.SOUTH, scrollTabla);
+        pnlRoot.setBackground(Color.white);
+        pnlRoot.add(BorderLayout.NORTH, pnlHead);
+        pnlRoot.add(BorderLayout.CENTER, scrollEditor);
+        pnlRoot.add(BorderLayout.EAST, pnlDer);
+        pnlRoot.add(BorderLayout.SOUTH, pnlBajo);
     }
     
+    private void initPnlBajo() {
+        pnlBajo.add(BorderLayout.CENTER, scrollTabla);
+    }
     
-    private Font inputFont() {
+    private Font addFont(String nombre, float tam) {
         Font font = null;
         try {
-            font = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/fonts/Input.ttf"));
-            font = font.deriveFont(15f);
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/fonts/" + nombre));
+            font = font.deriveFont(tam);
         } catch (FontFormatException | IOException ex) {
             Logger.getLogger(CompilerGUI.class.getName()).log(Level.SEVERE, null, ex);
         }
         return font;
     }
-
-    public JTextPane getTxPane() {
-        return txPane;
+    
+    
+    private Font inputFont() {
+        return addFont("Input.ttf", 15);
+    }
+    
+    private Font titleFont(float tam) {
+        return addFont("RobotoMono-Regular.ttf", tam);
     }
 
-    public DefaultListModel getModelList() {
-        return modelList;
+    public JTextPane getTxPane() {
+        return tpEditor;
+    }
+
+    public JTextPane getTpEditor() {
+        return tpEditor;
     }
 
     public DefaultTableModel getModelTable() {
@@ -174,10 +205,26 @@ public class CompilerGUI extends JFrame {
     public JButton getBtnErrores() {
         return btnErrores;
     }
-    
-    private void initStyles() {
-        
+
+    public void setOutConsola(String out) {
+        tpConsola.setForeground(Color.white);
+        this.tpConsola.setText(out);
     }
+    
+    public void setErrConsola(String err) {
+        tpConsola.setForeground(new Color(0xf88279));
+        this.tpConsola.setText(tpConsola.getText() + err);
+    }
+    
+    
+    private void estilosJScrollPanes(JScrollPane... scrollpanes) {
+        Arrays.asList(scrollpanes).forEach(scrollpane -> {
+            scrollpane.setBorder(null);
+            scrollpane.getVerticalScrollBar().setBlockIncrement(18);
+        });
+    }
+
+    
     
 
 }
